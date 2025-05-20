cadd107e2af67f6a032909a09d21030429f90ccc
Fernando <fernandomoraisdev@gmail.com>
2025-05-19 23:14:30 -0300

    funcao de deposito adicionada

--- banco.php
+++ banco.php
@@ -1,6 +1,22 @@
 <?php
-function exibeMensagem($mensagem) {
+function sacar($conta, $valorASacar)
+{
+    if($valorASacar > $conta["saldo"]){
+        echo "voce nao pode sacar esse valor" . PHP_EOL;
+    }else{
+        $conta["saldo"] -= $valorASacar;
+    }
+    return $conta;
+}
+
+function exibeMensagem($mensagem)
+{
     echo $mensagem . PHP_EOL;
+}
+
+function depositar($conta, $valorADepositar){
+    $conta["saldo"] += $valorADepositar;
+    return $conta;
 }
 
 $contaCorrente = [
@@ -10,28 +26,39 @@ $contaCorrente = [
     ],
     "123.456.789-11" => [
         "titular" => "Mikael Lucca",
-        "saldo" => 300,
+        "saldo" => 3000,
     ],
     "123.456.789-12" => [
         "titular" => "Neuzimar Santos",
-        "saldo" => 100,
+        "saldo" => 1000,
     ],
 ];
+$contaCorrente["123.456.789-10"] = sacar(
+    $contaCorrente["123.456.789-10"],
+    500
+);
+$contaCorrente["123.456.789-11"] = sacar(
+    $contaCorrente["123.456.789-11"],
+    500
+);
+$contaCorrente["123.456.789-12"] = sacar(
+    $contaCorrente["123.456.789-12"],
+    500
+);
 
 
-
-if(500 > $contaCorrente["123.456.789-10"]["saldo"]){
-    echo "voce nao pode sacar esse valor da conta do Mikael" . PHP_EOL;
-}else{
-    $contaCorrente ["123.456.789-10"]["saldo"] -= 500;
-}
-
-if(500 > $contaCorrente["123.456.789-11"]["saldo"]){
-    echo "voce nao pode sacar esse valor da conta do Mikael" . PHP_EOL;
-}else{
-    $contaCorrente ["123.456.789-11"]["saldo"] -= 500;
-}
-
+$contaCorrente["123.456.789-10"] = depositar(
+    $contaCorrente["123.456.789-10"],
+    700
+);
+$contaCorrente["123.456.789-11"] = depositar(
+    $contaCorrente["123.456.789-11"],
+    400
+);
+$contaCorrente["123.456.789-12"] = depositar(
+    $contaCorrente["123.456.789-12"],
+    5000
+);
 
 foreach ($contaCorrente as $cpf => $conta) {
     exibeMensagem($cpf . " " . $conta["titular"] . " " . $conta["saldo"]);
