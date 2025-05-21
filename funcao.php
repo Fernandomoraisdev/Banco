24c56178f65a94ff8b0fba1810cec8e32fdfc2bb
Fernando <fernandomoraisdev@gmail.com>
2025-05-21 20:41:50 -0300

    colocando letras maiusculas

--- funcao.php
+++ funcao.php
@@ -24,4 +24,7 @@ function depositar(array $conta, float $valorADepositar): array
     return $conta;
 }
 
-
+function titularComLetrasMaiusculas(array &$conta)
+{
+   $conta["titular"] = mb_strtoupper($conta['titular']);
+}
