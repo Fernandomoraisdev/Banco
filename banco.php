2d5f556083086459a082be66176aaf3a7342a8e1
Fernando <fernandomoraisdev@gmail.com>
2025-05-24 13:58:53 -0300

    codigo html adicionado

--- banco.php
+++ banco.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once 'funcao.php';
 
 $contaCorrente = [
@@ -50,11 +49,30 @@ titularComLetrasMaiusculas($contaCorrente ["123.456.789-12"]);
 
 unset($contaCorrente["123.456.789-10"]);
 
-foreach ($contaCorrente as $cpf => $conta) {
-    ['titular' => $titular, 'saldo' => $saldo] = $conta;
-    exibeMensagem(
-        "$cpf $titular $saldo"
-    );
+?>
 
-}
 
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas Correntes</h1>
+    <dl>
+        <?php foreach ($contaCorrente as $cpf => $conta) { ?>
+        <dt>
+            <h3><?=  $conta['titular'];?> - <?= $cpf; ?> </h3>
+        </dt>
+        <dd>
+            Saldo:<?= $conta['saldo'];?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
