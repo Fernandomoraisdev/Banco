cdfa1c75eff9afee3a66fc1283b9a101c0a3e497
fernandomoraisdev <fernandomoraisdev@gmail.com>
2025-05-29 16:16:51 -0300

    Methods end public add

---
+++ banco-antigo.php
@@ -0,0 +1,78 @@
+ <?php
+require_once 'funcao-antiga.php';
+
+$contaCorrente = [
+    "123.456.789-10" => [
+        "titular" => "Fernando Morais",
+        "saldo" => 10000,
+    ],
+    "123.456.789-11" => [
+        "titular" => "Mikael Lucca",
+        "saldo" => 3000,
+    ],
+    "123.456.789-12" => [
+        "titular" => "Neuzimar Santos",
+        "saldo" => 1000,
+    ],
+];
+
+$contaCorrente["123.456.789-10"] = sacar(
+    $contaCorrente["123.456.789-10"],
+    500
+);
+$contaCorrente["123.456.789-11"] = sacar(
+    $contaCorrente["123.456.789-11"],
+    500
+);
+$contaCorrente["123.456.789-12"] = sacar(
+    $contaCorrente["123.456.789-12"],
+    500
+);
+
+
+$contaCorrente["123.456.789-10"] = depositar(
+    $contaCorrente["123.456.789-10"],
+    70
+);
+$contaCorrente["123.456.789-11"] = depositar(
+    $contaCorrente["123.456.789-11"],
+    400
+);
+$contaCorrente["123.456.789-12"] = depositar(
+    $contaCorrente["123.456.789-12"],
+    5000
+);
+
+titularComLetrasMaiusculas($contaCorrente ["123.456.789-10"]);
+titularComLetrasMaiusculas($contaCorrente ["123.456.789-11"]);
+titularComLetrasMaiusculas($contaCorrente ["123.456.789-12"]);
+
+unset($contaCorrente["123.456.789-10"]);
+
+?>
+
+
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas Correntes</h1>
+    <dl>
+        <?php foreach ($contaCorrente as $cpf => $conta) { ?>
+        <dt>
+            <h3><?=  $conta['titular'];?> - <?= $cpf; ?> </h3>
+        </dt>
+        <dd>
+            Saldo:<?= $conta['saldo'];?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
--- src/Conta.php
+++ src/Conta.php
@@ -2,6 +2,57 @@
 class Conta
 {
     private string $cpfTitular;
-    public string $nomeTitular;
-    private float $saldo;
+    private string $nomeTitular;
+    private float $saldo = 0;
+
+    public function sacar(float $valorASacar): void
+    {
+        if($valorASacar > $this->saldo) {
+            echo "Saldo indisponivel";
+            return;
+        }
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponivel";
+            return;
+        }
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
+
+    public function recuperarSaldo (): float
+    {
+        return $this->saldo;
+    }
+
+    public function defineCpfTitular(string $cpf): void
+    {
+       $this -> cpfTitular;
+    }
+
+    public function defineNomeTitular(string $nome): void
+    {
+       $this -> nomeTitular = $nome;
+    }
+    public function recuperarCpfTitular (): string
+    {
+        return $this -> cpfTitular;
+    }
+    public function recuperarNomeTitular (): string
+    {
+        return $this -> nomeTitular;
+    }
 }
