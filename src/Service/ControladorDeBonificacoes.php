3792828e4c758110c65ca8e25e249f9d07f62ea9
Fernando Morais <71509217+Fernandomoraisdev@users.noreply.github.com>
2025-06-10 22:39:10 -0300

    Novo arquivo adicionado

--- src/Service/ControladorDeBonificacoes.php
+++ src/Service/ControladorDeBonificacoes.php
@@ -4,5 +4,14 @@ namespace Alura\Banco\Service;
 
 class ControladorDeBonificacoes
 {
+    private $totalBonificacao = 0;
+    public function adicionaBonificacaoDe(Funcionario $funcionario)
+    {
+        $this->totalBonificacao += $funcionario->calculaBonificacao();
+    }
 
+    public function recuperaTotal(): float
+    {
+        return $this->totalBonificacao;
+    }
 }
