cce6803ba9d022c41a846d25f5c49031ded8d5ff
Fernando Morais <71509217+Fernandomoraisdev@users.noreply.github.com>
2025-06-19 23:57:55 -0300

    login add

---
+++ autenticacao.php
@@ -0,0 +1,14 @@
+<?php
+
+use Alura\Banco\Model\CPF;
+use Banco\Service\Autenticador;
+
+require_once "autoload.php";
+
+$autenticador = new Autenticador();
+$funcionario3 = new Diretor("Aristoteles jones",
+    new CPF('123.456.789-35'),
+    10000
+);
+
+$autenticador->tentalogin($funcionario3, '4321');
--- src/Service/Autenticador.php
+++ src/Service/Autenticador.php
@@ -6,6 +6,10 @@ class Autenticador
 {
     public function tentaLogin (Diretor $diretor, string $senha): bool
     {
-
+        if ($diretor->autenticar($senha)){
+            echo "Ok. Usuario logado com sucesso";
+        }else{
+            "Ops. Senha incorreta";
+        }
     }
 }
