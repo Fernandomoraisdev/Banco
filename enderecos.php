6ffce0ff7b6500ee26ab1627fd93cefe013b4d4a
Fernando Morais <71509217+Fernandomoraisdev@users.noreply.github.com>
2025-06-21 22:53:25 -0300

    ainda com erro

--- enderecos.php
+++ enderecos.php
@@ -1,3 +1,8 @@
 <?php
 
+use Alura\Banco\Model\Endereco;
+
 require_once "autoload.php";
+
+$umEndereco = new Endereco("duque de caxias", "pilar", "feliz", "54");
+$outroEndereco = new Endereco("duque de caxias", "matriz", "malao",  "54");
